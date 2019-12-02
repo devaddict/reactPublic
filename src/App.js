93078b8b55e4ce881fc1349f639c3ab1410072d2
devaddict <devaddict@outlook.com>
2019-12-02 19:29:35 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+      <div className="App">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            https://reqres.in/ for rest apis
+          </p>
+      </div>
+    );
+}
+
+export default App;
---
+++ src/Utils/EndPoints.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+class DataActions {
+
+    constructor() {
+        const appUrl = 'https://jsonplaceholder.typicode.com'; // Wordpress installation url
+
+        this.usersEndPoint = `${appUrl}/users`; // Endpoint for getting Wordpress Pages
+        this.postsEndPoint = `${appUrl}/posts`; // Endpoint for getting Wordpress Posts
+    }
+
+    // Method for getting data from the provided end point url
+    api(endPoint) {
+        return new Promise((resolve, reject) => {
+            axios.get(endPoint).then((response) => {
+                resolve(response.data);
+            }).catch((error) => {
+                reject(error);
+            }); 
+        });     
+    }
+
+    // Method for getting Pages data
+    getUsers(cb){
+        this.api(this.pagesEndPoint).then((response)=>{
+            this.getPosts(response, cb)
+        });
+        return true;
+    }
+
+    // Method for getting Posts data
+    getPosts(pages, cb){
+        this.api(this.postsEndPoint).then((response)=>{
+            const posts     = response
+            const payload   = { pages, posts };
+
+            this.getSuccess(payload); // Pass returned data to the store
+            cb(payload); // This callback will be used for dynamic rout building
+        });
+        return true;
+    }
+
+    // This returnes an object with Pages and Posts data together
+    // The Alt Store will listen for this method to fire and will store the returned data
+    getSuccess(payload){
+        return payload;
+    }
+}
